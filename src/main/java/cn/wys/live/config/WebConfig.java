306a22da5e9daad843ae82a3a9816efc87bcf84f
wyscoder <2900086967@qq.com>
2019-12-27 10:55:04 +0800

    添加了登录页面的拦截

---
+++ src/main/java/cn/wys/live/config/WebConfig.java
@@ -0,0 +1,31 @@
+package cn.wys.live.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 拦截器统一管理
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login").excludePathPatterns("/register").excludePathPatterns("/static/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
+
+
+
--- src/main/java/cn/wys/live/controller/LoginController.java
+++ src/main/java/cn/wys/live/controller/LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * @author wys
  * @date 2019/12/26
@@ -22,11 +24,13 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String Login(String username,String password){
+    public String Login(String username, String password, HttpSession session){
 
         User user = userMapper.selectUserByUserName(username);
         System.out.println(user);
         if(user.getPassword().equals(password)){
+            session.setAttribute("name",user.getName());
+            session.setAttribute("id",user.getId());
             return "index";
         }
         return "login";
